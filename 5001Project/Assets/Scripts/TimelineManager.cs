36cb98b7ef85fefd0c910eb443335d003422faad
rcooper3307 <rcooper3307@bths.edu>
2023-04-16 20:37:56 -0400

    added progress bar to both scenes

--- 5001Project/Assets/Scripts/TimelineManager.cs
+++ 5001Project/Assets/Scripts/TimelineManager.cs
@@ -11,6 +11,8 @@ public class TimelineManager : MonoBehaviour
     [SerializeField] private List<TimelinePiece> pieces;
     [SerializeField] private List<TimelinePiece> piecesSelected = new List<TimelinePiece>();
     [SerializeField] PersistentData p;
+    ProgressScript progress;
+
     public GameObject pieceList;
     public int pieceSeries;
     public int[] givenvalues = new int[2];
@@ -29,7 +31,9 @@ public class TimelineManager : MonoBehaviour
         SpawnSlots();
         SpawnPiece();
         p = FindObjectOfType<PersistentData>();
-        
+        progress = FindObjectOfType<ProgressScript>();
+
+
     }
     
     //Initial Slot Spawner
@@ -71,71 +75,97 @@ public class TimelineManager : MonoBehaviour
 public void Proceed()
     {
 
+       
+
+        //Post Game Section
+        if (!PersistentData.Instance.GameStatus())
+        {
+
+            progress.AddProgress();
+            StartCoroutine(SceneDelay("PickOne",3f));
+
+        }
+
+        else
+        {
+            progress.AddProgress();
+            StartCoroutine(SceneDelay("MainMenu", 3f));
+        }
+            
+    }
+    //delays the transition to scene 'scene' by float 'time'
+    IEnumerator SceneDelay(string scene, float time)
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(time);
         //Cleanup Section
         piecesSelected[2].deactivate();
         piecesSelected[2].transform.position = selectedLoc.position;
         piecesSelected[0].transform.position = selectedLoc.position;
         piecesSelected[1].transform.position = selectedLoc.position;
+        SceneManager.LoadScene(scene);
 
-        //Post Game Section
-        if (!PersistentData.Instance.GameStatus())
-            SceneManager.LoadScene("PickOne");
-        else
-            SceneManager.LoadScene("MainMenu");
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        // Code to execute after 3 second delay
     }
-/*
-    public void Restart()
-    {
-        
-        //Reset Section
-        int pieceindex = (piecesSelected.Count)-1;
 
-        piecesSelected[pieceindex].deactivate();
-        piecesSelected[pieceindex].transform.position = selectedLoc.position;
-        piecesSelected[selectedIndex[0]].transform.position = selectedLoc.position;
-        piecesSelected[selectedIndex[1]].transform.position = selectedLoc.position;
-
-        //Restart Section
-        if (pieces.Count > 0) //checks if there are any unselected pieces left
+    /*
+        public void Restart()
         {
-            //randomly selects two indexes from selected list
-            int listsize = piecesSelected.Count;
-            int rand1 = Random.Range(0,listsize-1);
-            int rand2 = Random.Range(0,listsize-1);
-            while(rand1 == rand2) //to make sure rand1 != rand2
-                rand2 = Random.Range(0,listsize-1);
 
-            //assigns the two index selected to selectedIndex array and their values to givenvalues array
-            givenvalues[0] = piecesSelected[rand1].getValue();
-            selectedIndex[0] = rand1;
+            //Reset Section
+            int pieceindex = (piecesSelected.Count)-1;
 
-            givenvalues[1] = piecesSelected[rand2].getValue();
-            selectedIndex[1] = rand2;
+            piecesSelected[pieceindex].deactivate();
+            piecesSelected[pieceindex].transform.position = selectedLoc.position;
+            piecesSelected[selectedIndex[0]].transform.position = selectedLoc.position;
+            piecesSelected[selectedIndex[1]].transform.position = selectedLoc.position;
 
-            if(givenvalues[1] > givenvalues[0]) //condition of value of index 1 piece > index 0 piece 
+            //Restart Section
+            if (pieces.Count > 0) //checks if there are any unselected pieces left
             {
-                piecesSelected[rand1].transform.position = shownSpawnLoc.GetChild(0).position;
-                piecesSelected[rand2].transform.position = shownSpawnLoc.GetChild(1).position; 
-                Debug.Log("LEFT: " + givenvalues[0] + "  RIGHT: " + givenvalues[1]);
-                LGR = false;
-            }
-            else //vice versa
-            {
-                piecesSelected[rand1].transform.position = shownSpawnLoc.GetChild(1).position;
-                piecesSelected[rand2].transform.position = shownSpawnLoc.GetChild(0).position;
-                Debug.Log("LEFT: " + givenvalues[1] + "  RIGHT: " + givenvalues[0]);
-                LGR = true;
-            }
+                //randomly selects two indexes from selected list
+                int listsize = piecesSelected.Count;
+                int rand1 = Random.Range(0,listsize-1);
+                int rand2 = Random.Range(0,listsize-1);
+                while(rand1 == rand2) //to make sure rand1 != rand2
+                    rand2 = Random.Range(0,listsize-1);
 
-            //calls SpawnPiece to select new piece to place on timeline
-            SpawnPiece();
+                //assigns the two index selected to selectedIndex array and their values to givenvalues array
+                givenvalues[0] = piecesSelected[rand1].getValue();
+                selectedIndex[0] = rand1;
+
+                givenvalues[1] = piecesSelected[rand2].getValue();
+                selectedIndex[1] = rand2;
+
+                if(givenvalues[1] > givenvalues[0]) //condition of value of index 1 piece > index 0 piece 
+                {
+                    piecesSelected[rand1].transform.position = shownSpawnLoc.GetChild(0).position;
+                    piecesSelected[rand2].transform.position = shownSpawnLoc.GetChild(1).position; 
+                    Debug.Log("LEFT: " + givenvalues[0] + "  RIGHT: " + givenvalues[1]);
+                    LGR = false;
+                }
+                else //vice versa
+                {
+                    piecesSelected[rand1].transform.position = shownSpawnLoc.GetChild(1).position;
+                    piecesSelected[rand2].transform.position = shownSpawnLoc.GetChild(0).position;
+                    Debug.Log("LEFT: " + givenvalues[1] + "  RIGHT: " + givenvalues[0]);
+                    LGR = true;
+                }
+
+                //calls SpawnPiece to select new piece to place on timeline
+                SpawnPiece();
+            }
+            else //condition for when all the pieces are selected
+            {
+                Debug.Log("FINISHED");
+            }
         }
-        else //condition for when all the pieces are selected
-        {
-            Debug.Log("FINISHED");
-        }
-    }
-*/
+    */
     public void InitializationProcess()
     {
         int piecelistsize = 0;
