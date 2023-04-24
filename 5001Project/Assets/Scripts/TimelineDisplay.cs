2a0d9850bc6b5c2a05ddc49a3dee0a4865fef2f8
Swann Thantsin <swannxyz@gmail.com>
2023-04-24 04:14:15 -0400

    timeline revamped 0.1
    ~Changed timeline to represent an actual timeline
    *not fully functional as of 4/24/2023

--- 5001Project/Assets/Scripts/TimelineDisplay.cs
+++ 5001Project/Assets/Scripts/TimelineDisplay.cs
@@ -9,23 +9,37 @@ public class TimelineDisplay : MonoBehaviour
 {
     [SerializeField] GameObject Timeline;
     [SerializeField] private List<GameObject> ElementsToPause;
-    [SerializeField] private Transform displayLoc, outofFrameLoc;
     [SerializeField] private List<TimelinePiece> pieces;
+    [SerializeField] private Transform timelineSlots;
+    [SerializeField] private Transform outOfDisplay;
+    [SerializeField] public List<Text> textField;
+    [SerializeField] public List<GameObject> previousSpots = new List<GameObject>();
     public GameObject pieceList;
     public int piecelistsize;
-    public int currentpiece;
-    public Text titleField;
     public string titlestring;
+    public int seriesCount;
+    public int currentpage = 1;
+    public int pageCount;
 
     void Awake()
     {
-        pieceList = GameObject.FindGameObjectWithTag("pieceList");
+        StoreLocation();
+        LoadLocation();
+        StoreLocation();
+        LoadLocation();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        pieceList = GameObject.FindGameObjectWithTag("pieceList");
+        piecelistsize = pieceList.transform.childCount;
 
+        TimelinePiece placeholder;
+        placeholder = pieceList.GetComponentInChildren<TimelinePiece>(piecelistsize - 1);
+        seriesCount = placeholder.getSeries();
+        pageCount = seriesCount;
+        Debug.Log(pageCount);
     }
 
     // Update is called once per frame
@@ -36,74 +50,145 @@ public class TimelineDisplay : MonoBehaviour
 
     public void GoRight()
     {
-        if(currentpiece != piecelistsize - 1)
+        if(currentpage != pageCount)
         {
-            currentpiece++;
-            titlestring = pieces[currentpiece].GetComponentInChildren<TextMeshPro>().text;
-            titleField.text = titlestring; 
+            currentpage++;
+            CreateTimeline();
         }
         else
         {
-            Debug.Log("There is nothing on the right of this piece!");
+            Debug.Log("No more pages on the right!");
         }
     }
 
     public void GoLeft()
     {
-        if(currentpiece != 0)
+        if(currentpage != 1)
         { 
-            currentpiece--;
-            titlestring = pieces[currentpiece].GetComponentInChildren<TextMeshPro>().text;
-            titleField.text = titlestring;
+            currentpage--;
+            CreateTimeline();
         }
         else
         {
-            Debug.Log("There is nothing on the left of this piece!");
+            Debug.Log("No more pages on the left!");
         }
     }
 
     public void DisplayTimeline()
     {
+        StoreLocation();
         for (int i = 0; i < ElementsToPause.Count; i++)
         {
             ElementsToPause[i].SetActive(false);
         }
         Timeline.SetActive(true);
-
         Time.timeScale = 0f;
 
-        pieceList = GameObject.FindGameObjectWithTag("pieceList");
-        piecelistsize = pieceList.transform.childCount;
+        CreateTimeline();
+    }
+    
+    public void Return()
+    {
+        Time.timeScale = 1f;
+        for (int i = 0; i < ElementsToPause.Count; i++)
+        {
+            ElementsToPause[i].SetActive(true);
+        }
+        Timeline.SetActive(false);
+        LoadLocation();
+    }
+    
+    public void StoreLocation()
+    {
+        for (int i = 0; i < piecelistsize; i++)
+        {
+            TimelinePiece placeholder;
+            placeholder = pieceList.GetComponentInChildren<TimelinePiece>(i);
+            
+            GameObject placeholderTransform = new GameObject();
+            placeholderTransform.transform.position = placeholder.transform.position;
+            previousSpots.Add(placeholderTransform);
+        }
+    }
 
-        currentpiece = 0;
+    public void LoadLocation()
+    {
+        for (int i = 0; i < piecelistsize; i++)
+        {
+            TimelinePiece placeholder;
+            placeholder = pieceList.GetComponentInChildren<TimelinePiece>(i);
+            placeholder.transform.position = previousSpots[i].transform.position;
+        }
+
+        foreach(GameObject obj in previousSpots)
+        {
+            Destroy(obj);
+        }
+        previousSpots.Clear();
+    }
+
+    public void CreateTimeline()
+    {
+         for (int i = 0; i < piecelistsize; i++)
+        {
+            TimelinePiece placeholder;
+            placeholder = pieceList.GetComponentInChildren<TimelinePiece>(i);
+
+            placeholder.transform.position = outOfDisplay.position;
+        }
+
+        for (int i = 0; i < 6; i++)
+        {
+            textField[i].text = "";
+        }
 
         for (int i = 0; i < piecelistsize; i++)
         {
             TimelinePiece placeholder;
             placeholder = pieceList.GetComponentInChildren<TimelinePiece>(i);
             if (placeholder.isPicked())
-                pieces.Add(placeholder);
+            {
+                if (placeholder.getSeries() == currentpage)
+                {
+                    switch(i%3)
+                    {
+                        case 0:
+                            placeholder.transform.position = timelineSlots.GetChild(0).position;
+                            textField[0].text = placeholder.GetComponentInChildren<TextMeshPro>().text;
+                            break;
+                        case 1:
+                            placeholder.transform.position = timelineSlots.GetChild(1).position;
+                            textField[1].text = placeholder.GetComponentInChildren<TextMeshPro>().text;
+                            break;
+                        case 2:
+                            placeholder.transform.position = timelineSlots.GetChild(2).position;
+                            textField[2].text = placeholder.GetComponentInChildren<TextMeshPro>().text;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else if (placeholder.getSeries() == currentpage + 1)
+                {
+                    switch(i%3)
+                    {
+                        case 0:
+                            placeholder.transform.position = timelineSlots.GetChild(3).position;
+                            textField[3].text = placeholder.GetComponentInChildren<TextMeshPro>().text;
+                            break;
+                        case 1:
+                            placeholder.transform.position = timelineSlots.GetChild(4).position;
+                            textField[4].text = placeholder.GetComponentInChildren<TextMeshPro>().text;
+                            break;
+                        case 2:
+                            placeholder.transform.position = timelineSlots.GetChild(5).position;
+                            textField[5].text = placeholder.GetComponentInChildren<TextMeshPro>().text;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
         }
-
-        piecelistsize = pieces.Count;
-
-        titlestring = pieces[currentpiece].GetComponentInChildren<TextMeshPro>().text;
-        titleField.text = titlestring;
-
-        pieceList.SetActive(false);
     }
-    
-    public void Return()
-    {
-        for (int i = 0; i < ElementsToPause.Count; i++)
-        {
-            ElementsToPause[i].SetActive(true);
-        }
-        pieceList.SetActive(true);
-
-        Time.timeScale = 1f;
-
-        Timeline.SetActive(false);
-    }
-    
 }
